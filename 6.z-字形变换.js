0a339be03d026422eecefec8ac584a01cecc486d
huarxia <liubiao0810@live.cn>
2020-11-03 18:25:59 +0800

    feat: 🥪 新增0006.z-字形变换算法题解

---
+++ 6.z-字形变换.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=javascript
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    const len = s.length;
+    // 如果字符长度小于小于行数或者传入行数为1则直接返回即可
+    if (numRows === 1 || s.length < numRows) {
+        return s;
+    }
+    /**
+     *  L     D     R
+        E   O E   I I
+        E C   I H   N
+        T     S     G
+     */
+    // 1. 先分析出多少个元素一次循环
+    // 2. 可以看到，没次循环时候，只有首尾是一个元素，其他行都是两个元素
+    // 3. 所以: 2 * (numRows - 2) + 2 ==> 2 * numRows -2 ===> loopNum
+    // 以4行为例得到 loopNum = 6 这个个数值表示多少个元素后循环往下排
+    // 4. 再次分析，每个元素应该放在哪个数组中，嘻嘻~~
+    // 5. 以第二列 C 为例 下标为 4, 但是应该放在下标为 2 的数组中
+    // 那么：4 % 6 = 4， 6 - 4 = 2；
+    // 6. 验证： 第6列I   11 % 6 = 5， 6 - 11 % 6 = 1
+    // 7. 嘻嘻: 所以 ===>
+    // 放置数组下标 = min(下标 % loopNum, loopNum - (下标 % loopNum));
+    // 生成一个长度为numRows的数组，每个数组中元素存放每行的数据拼接
+    const arr = new Array(numRows).fill('');
+    for (let i = 0; i < numRows; i++) {
+        
+    }
+};
+// @lc code=end
+
