5b3d9ea9b6e8bed1b85d6be9a67ed10874faf2d5
huarxia <liubiao0810@live.cn>
2020-11-03 15:46:49 +0800

    feat: 🥩 新增0002两数相加链表算法

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,8 +17,36 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    
+let addTwoNumbers = function(l1, l2) {
+    // 首先构造一个空链表，后面只需return ListNode.next即可
+    let List = new ListNode(0);
+    let head = List;
+    // 初始每位的相加和
+    let sum = 0;
+    // 进位标识
+    let carry = 0;
+    while (l1 !== null || l2 !== null || sum > 0) {
+        if (l1 !== null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 !== null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        if (sum >= 10) {
+            carry = 1;
+            sum -= 10;
+        }
+        // 连接链表
+        head.next = new ListNode(sum);
+        head = head.next;
+        // 进位1保留赋给sum方便下一步相加
+        sum = carry;
+        // 清空进位
+        carry = 0;
+    }
+    return List.next;
 };
 // @lc code=end
 
