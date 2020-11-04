4acaaadfdab6ac9afc8d13d3d4b670f0b52c2f64
huarxia <liubiao0810@live.cn>
2020-11-04 14:44:11 +0800

    feat: 🥩新增0006 Z 字形变换算法

--- 6.z-字形变换.js
+++ 6.z-字形变换.js
@@ -13,7 +13,7 @@
 var convert = function(s, numRows) {
     const len = s.length;
     // 如果字符长度小于小于行数或者传入行数为1则直接返回即可
-    if (numRows === 1 || s.length < numRows) {
+    if (numRows === 1 || len < numRows) {
         return s;
     }
     /**
@@ -23,7 +23,7 @@ var convert = function(s, numRows) {
         T     S     G
      */
     // 1. 先分析出多少个元素一次循环
-    // 2. 可以看到，没次循环时候，只有首尾是一个元素，其他行都是两个元素
+    // 2. 可以看到，每次次循环时候，只有首尾是一个元素，其他行都是两个元素
     // 3. 所以: 2 * (numRows - 2) + 2 ==> 2 * numRows -2 ===> loopNum
     // 以4行为例得到 loopNum = 6 这个个数值表示多少个元素后循环往下排
     // 4. 再次分析，每个元素应该放在哪个数组中，嘻嘻~~
@@ -34,9 +34,16 @@ var convert = function(s, numRows) {
     // 放置数组下标 = min(下标 % loopNum, loopNum - (下标 % loopNum));
     // 生成一个长度为numRows的数组，每个数组中元素存放每行的数据拼接
     const arr = new Array(numRows).fill('');
-    for (let i = 0; i < numRows; i++) {
-        
+    // 循环元素个数
+    const loopNum = 2 * numRows - 2;
+    for (let i = 0; i < len; i++) {
+        // 获取放置数组的下标
+        const index = Math.min(i % loopNum, loopNum - i % loopNum);
+        // 每一行的字符连接起来
+        arr[index] += s[i];
     }
+    // 直接转换为字符串返回即可
+    return arr.join('');
 };
 // @lc code=end
 
