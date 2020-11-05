6b1deb10ac5aeadcb0b52704c84014ac14727645
huarxia <liubiao0810@live.cn>
2020-11-05 16:28:45 +0800

    feat: 🍛 新增0009.回文数算法题解

---
+++ 9.回文数.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=javascript
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const str = String(x);
+    let first = str;
+    let last = str.split('').reverse().join('');
+    return first === last;
+};
+// @lc code=end
