ecda081a5dd1bcf122269ed7705ee1edd02c431e
huarxia <liubiao0810@live.cn>
2020-11-16 09:24:43 +0800

    feat: 🍝 新增0024.两两交换链表中的节点算法题解

---
+++ 24.两两交换链表中的节点.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    let n1 = head;
+    let n2 = n1.next;
+    let n3 = n2. next;
+    n2.next = n1;
+    n1.next = swapPairs(n3);
+    return n2;
+};
+// @lc code=end
+
