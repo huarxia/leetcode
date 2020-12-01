1f38e94b4f140f245e4c0ce6fc1a4672932076a0
huarxia <liubiao0810@live.cn>
2020-12-01 11:00:24 +0800

    feat: 🌼 新增0039.组合总和算法题解

---
+++ 0039.组合总和.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=javascript
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let buffer = [];
+    let result = [];
+    search(0, target);
+    return result;
+    function search(startIdx, target) {
+        if (target === 0) {
+            return result.push(buffer.slice());
+        }
+        if (target < 0) {
+            return;
+        }
+        if (startIdx === candidates.length) {
+            return;
+        }
+        buffer.push(candidates[startIdx]);
+        search(startIdx, target - candidates[startIdx]);
+        buffer.pop();
+        search(startIdx + 1, target);
+    }
+};
+// @lc code=end
+
